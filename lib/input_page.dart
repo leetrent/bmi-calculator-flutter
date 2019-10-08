5fd0cb058c115118c2a4794334f5d35be0bec824
LeeTrent <lee.trent.1@gmail.com>
2019-10-08 07:47:07 -0500

    Creates and uses 'ReusableCard' widget which is a custom Container widget

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -18,25 +18,25 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: ReusableCard(),
+                  child: ReusableCard(colour: Color(0xFF1D1E33)),
                 ),
                 Expanded(
-                  child: ReusableCard(),
+                  child: ReusableCard(colour: Color(0xFF1D1E33)),
                 ),
               ],
             ),
           ),
           Expanded(
-            child: ReusableCard(),
+            child: ReusableCard(colour: Color(0xFF1D1E33)),
           ),
           Expanded(
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: ReusableCard(),
+                  child: ReusableCard(colour: Color(0xFF1D1E33)),
                 ),
                 Expanded(
-                  child: ReusableCard(),
+                  child: ReusableCard(colour: Color(0xFF1D1E33)),
                 ),
               ],
             ),
@@ -48,16 +48,14 @@ class _InputPageState extends State<InputPage> {
 }
 
 class ReusableCard extends StatelessWidget {
-  const ReusableCard({
-    Key key,
-  }) : super(key: key);
-
+  ReusableCard({@required this.colour});
+  final Color colour;
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15.0),
       decoration: BoxDecoration(
-        color: Color(0xFF1D1E33),
+        color: colour,
         borderRadius: BorderRadius.circular(10.0),
       ),
     );
