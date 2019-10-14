4d88c5d820fd0262ebc44bb60e4cb9d110bc8106
LeeTrent <lee.trent.1@gmail.com>
2019-10-14 09:04:55 -0500

    Adds gender enum

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -6,7 +6,10 @@ import 'reusable_card.dart';
 
 const double bottomContainerHeight = 80.0;
 const Color activeCardColor = Color(0xFF1D1E33);
+const Color inactiveCardColor = Color(0xFF111328);
 const Color bottomContainerColor = Color(0xFFEB1555);
+
+enum Gender { male, female }
 
 class InputPage extends StatefulWidget {
   @override
