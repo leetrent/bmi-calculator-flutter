7e256c70d00712bf4d4861e6cf6801bdb608bcd9
LeeTrent <lee.trent.1@gmail.com>
2019-10-14 21:05:40 -0500

    1. Moves GestureDetector to ResuableCard; 2.Adds an onPress function as a member of the ResusableCard class;

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -34,38 +34,34 @@ class _InputPageState extends State<InputPage> {
             child: Row(
               children: <Widget>[
                 Expanded(
-                  child: GestureDetector(
-                    onTap: () {
+                  child: ReusableCard(
+                    onPress: () {
                       setState(() {
                         selectedGender = Gender.male;
                       });
                     },
-                    child: ReusableCard(
-                      colour: selectedGender == (Gender.male)
-                          ? (activeCardColor)
-                          : (inactiveCardColor),
-                      childChild: IconContent(
-                        iconData: FontAwesomeIcons.mars,
-                        label: 'MALE',
-                      ),
+                    colour: selectedGender == (Gender.male)
+                        ? (activeCardColor)
+                        : (inactiveCardColor),
+                    childChild: IconContent(
+                      iconData: FontAwesomeIcons.mars,
+                      label: 'MALE',
                     ),
                   ),
                 ),
                 Expanded(
-                  child: GestureDetector(
-                    onTap: () {
+                  child: ReusableCard(
+                    onPress: () {
                       setState(() {
                         selectedGender = Gender.female;
                       });
                     },
-                    child: ReusableCard(
-                      colour: selectedGender == (Gender.female)
-                          ? (activeCardColor)
-                          : (inactiveCardColor),
-                      childChild: IconContent(
-                        iconData: FontAwesomeIcons.venus,
-                        label: 'FEMALE',
-                      ),
+                    colour: selectedGender == (Gender.female)
+                        ? (activeCardColor)
+                        : (inactiveCardColor),
+                    childChild: IconContent(
+                      iconData: FontAwesomeIcons.venus,
+                      label: 'FEMALE',
                     ),
                   ),
                 ),
--- lib/reusable_card.dart
+++ lib/reusable_card.dart
@@ -1,17 +1,22 @@
 import 'package:flutter/material.dart';
 
 class ReusableCard extends StatelessWidget {
-  ReusableCard({@required this.colour, this.childChild});
+  ReusableCard({@required this.colour, this.childChild, this.onPress});
   final Color colour;
   final Widget childChild;
+  final Function onPress;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: this.childChild,
-      margin: EdgeInsets.all(15.0),
-      decoration: BoxDecoration(
-        color: colour,
-        borderRadius: BorderRadius.circular(10.0),
+    return GestureDetector(
+      onTap: onPress,
+      child: Container(
+        child: this.childChild,
+        margin: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+          color: colour,
+          borderRadius: BorderRadius.circular(10.0),
+        ),
       ),
     );
   }
