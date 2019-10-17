41ccec80c3a173ab829947e3873423ef7a8fd2b0
LeeTrent <lee.trent.1@gmail.com>
2019-10-17 07:23:45 -0500

    Wraps FloatingActionButton in a Row and adds a second FloatingActionButton

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -137,12 +137,27 @@ class _InputPageState extends State<InputPage> {
                           weight.toString(),
                           style: kNumberTextStyle,
                         ),
-                        FloatingActionButton(
-                          backgroundColor: Color(0xFF4C4F5E),
-                          child: Icon(
-                            Icons.add,
-                            color: Colors.white,
-                          ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: <Widget>[
+                            FloatingActionButton(
+                              backgroundColor: Color(0xFF4C4F5E),
+                              child: Icon(
+                                Icons.add,
+                                color: Colors.white,
+                              ),
+                            ),
+                            SizedBox(
+                              width: 10.0,
+                            ),
+                            FloatingActionButton(
+                              backgroundColor: Color(0xFF4C4F5E),
+                              child: Icon(
+                                Icons.add,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
                         )
                       ],
                     ),
