337d507db80e1396622f02cc3127a4098e6c4462
ItsTimeToGetOut <43565190+ItsTimeToGetOut@users.noreply.github.com>
2019-10-23 02:35:19 +0530

    for hacktoberfest

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
 const double kBottomContainerHeight = 80.0;
-const Color kActiveCardColor = Color(0xFF1D1E33);
-const Color kInactiveCardColor = Color(0xFF111328);
-const Color kBottomContainerColor = Color(0xFFEB1555);
+const Color kActiveCardColor = Colors.green;
+const Color kInactiveCardColor = Color.grey;
+const Color kBottomContainerColor = Color.blue;
 
 const kLabelTextStyle = TextStyle(
   fontSize: 18.0,
